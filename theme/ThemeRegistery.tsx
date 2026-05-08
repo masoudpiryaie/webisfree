49a8c7e5b315e88efec34febc49712a9a6247e38
masoud <fornt.react.dev@gmail.com>
2026-05-08 22:29:34 +0200

    fix theme

--- theme/ThemeRegistery.tsx
+++ theme/ThemeRegistery.tsx
@@ -3,12 +3,12 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { theme } from "@/theme/theme";
-
+import { getMuiTheme } from "@/theme/theme";
+//
 type ThemeRegistryProps = {
   children: React.ReactNode;
 };
-
+const theme = getMuiTheme("dark");
 export default function ThemeRegistry({ children }: ThemeRegistryProps) {
   return (
     <AppRouterCacheProvider>
